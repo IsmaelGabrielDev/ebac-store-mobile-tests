1cc102f057ef692eb0d97f042910c2a9e33b7daa
Ismael Gabriel <ismaelgabriel31@gmail.com>
2024-11-05 21:07:23 -0300

    Mudança no codigo

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -2,6 +2,7 @@ import { expect, driver } from '@wdio/globals'
 import homePage from '../pageobjects/home.page.js'
 import loginPage from '../pageobjects/login.page.js'
 import profilePage from '../pageobjects/profile.page.js'
+
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await homePage.openMenu('profile')
